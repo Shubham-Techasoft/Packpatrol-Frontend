04c92b064019a97ba445908adf119b99704cd95a
Nandini <bhardwajnandini4@gmail.com>
2025-06-18 12:45:07 +0530

    updated PNG issue

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,12 @@ import TechasoftHero1 from "./Techasoft_hero1.png";
 import TechasoftHero2 from "./Techasoft_hero2.png";
 import TechasoftHero3 from "./Techasoft_hero3.png";
 import ContactImage from "./ContactImage.png"
-import T1 from "./T1.png"
-import T2 from "./T2.png"
-import T3 from "./T3.png"
-import T4 from "./T4.png"
-import T5 from "./T5.png"
-import T6 from "./T6.png"
+import T1 from "./T1.PNG"
+import T2 from "./T2.PNG"
+import T3 from "./T3.PNG"
+import T4 from "./T4.PNG"
+import T5 from "./T5.PNG"
+import T6 from "./T6.PNG"
 
 export const heroImages = [TechasoftHero1, TechasoftHero2, TechasoftHero3,ContactImage];
 
